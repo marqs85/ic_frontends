a3f887efd9675455b6cc942e10527623750bc42c
marqs <mhiienka@niksula.hut.fi>
2023-08-20 23:00:24 +0300

    isl51002: add support for Reverse LPF

--- isl51002/isl51002_frontend.v
+++ isl51002/isl51002_frontend.v
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2019-2020  Markus Hiienkari <mhiienka@niksula.hut.fi>
+// Copyright (C) 2019-2023  Markus Hiienkari <mhiienka@niksula.hut.fi>
 //
 // This file is part of Open Source Scan Converter project.
 //
@@ -38,6 +38,7 @@ module isl51002_frontend (
     input [31:0] hv_in_config,
     input [31:0] hv_in_config2,
     input [31:0] hv_in_config3,
+    input [31:0] misc_config,
     output [7:0] R_o,
     output [7:0] G_o,
     output [7:0] B_o,
@@ -64,8 +65,14 @@ localparam VSYNC_RAW = 1'b1;
 localparam CSC_YCBCR601_RGB = 1'b0;
 localparam CSC_YCBCR709_RGB = 1'b1;
 
-localparam PP_PL_START  = 1;
-localparam PP_PL_END    = 4;
+localparam PP_PL_START      = 1;
+localparam PP_CSC_START     = 0;
+localparam PP_CSC_LENGTH    = 5;
+localparam PP_CSC_END       = PP_CSC_START + PP_CSC_LENGTH;
+localparam PP_RLPF_START    = PP_CSC_END;
+localparam PP_RLPF_LENGTH   = 3;
+localparam PP_RLPF_END      = PP_RLPF_START + PP_RLPF_LENGTH;
+localparam PP_PL_END        = PP_RLPF_END;
 
 reg [11:0] h_cnt;
 reg [10:0] v_cnt;
@@ -87,12 +94,17 @@ reg datavalid_pp[PP_PL_START:PP_PL_END] /* synthesis ramstyle = "logic" */;
 reg [10:0] xpos_pp[PP_PL_START:PP_PL_END] /* synthesis ramstyle = "logic" */;
 reg [10:0] ypos_pp[PP_PL_START:PP_PL_END] /* synthesis ramstyle = "logic" */;
 
+// Reverse LPF
+wire rlpf_trigger_act;
+reg signed [14:0] R_diff_s15_pre, G_diff_s15_pre, B_diff_s15_pre, R_diff_s15, G_diff_s15, B_diff_s15;
+reg [7:0] R_pp_prev_rlpf, G_pp_prev_rlpf, B_pp_prev_rlpf;
+
 // Measurement registers
 reg [20:0] pcnt_frame_ctr;
 reg [11:0] pcnt_line, pcnt_line_ctr, meas_h_cnt;
 reg pcnt_line_stored;
 reg [10:0] meas_v_cnt;
-reg meas_hl_det, meas_fid;
+reg meas_vsync_region, meas_hl_det, meas_fid;
 
 wire [11:0] H_TOTAL = hv_in_config[11:0];
 wire [11:0] H_ACTIVE = hv_in_config[23:12];
@@ -102,6 +114,9 @@ wire [8:0] H_BACKPORCH = hv_in_config2[8:0];
 wire [10:0] V_ACTIVE = hv_in_config2[30:20];
 wire [3:0] V_SYNCLEN = hv_in_config3[3:0];
 wire [8:0] V_BACKPORCH = hv_in_config3[12:4];
+
+wire [5:0] MISC_REV_LPF_STR = (misc_config[11:7] + 6'd16);
+wire MISC_REV_LPF_ENABLE = (misc_config[11:7] != 5'h0);
 
 wire [11:0] even_min_thold_hv = (H_TOTAL / 12'd4);
 wire [11:0] even_max_thold_hv = (H_TOTAL / 12'd2) + (H_TOTAL / 12'd4);
@@ -113,7 +128,7 @@ wire [11:0] even_max_thold = (vsync_i_type == VSYNC_SEPARATED) ? even_max_thold_
 wire [11:0] meas_even_min_thold = (vsync_i_type == VSYNC_SEPARATED) ? (pcnt_line / 12'd2) : (pcnt_line / 12'd4);
 wire [11:0] meas_even_max_thold = (vsync_i_type == VSYNC_SEPARATED) ? pcnt_line : (pcnt_line / 12'd2) + (pcnt_line / 12'd4);
 
-wire meas_vblank_region = ((pcnt_frame_ctr < (pcnt_frame/8)) | (pcnt_frame_ctr > (pcnt_frame - (pcnt_frame/8))));
+wire meas_vblank_region = ((pcnt_frame_ctr < (pcnt_frame/16)) | (pcnt_frame_ctr > (pcnt_frame - (pcnt_frame/16))));
 wire [11:0] glitch_filt_thold = meas_vblank_region ? (pcnt_line/4) : (pcnt_line/8);
 
 // TODO: calculate H/V polarity independently
@@ -141,6 +156,17 @@ reg [7:0] R_csc, G_csc, B_csc;
 
 wire [35:0] R_Cr_pre, G_Cb_pre, G_Cr_pre, B_Cb_pre;
 wire [10:0] R_csc_sum, G_csc_sum, B_csc_sum;
+
+function [7:0] apply_reverse_lpf;
+    input [7:0] data_prev;
+    input signed [14:0] diff;
+    reg signed [10:0] result;
+
+    begin
+        result = {3'b0,data_prev} + ~diff[14:4]; // allow for a small error to reduce adder length
+        apply_reverse_lpf = result[10] ? 8'h00 : |result[9:8] ? 8'hFF : result[7:0];
+    end
+endfunction
 
 
 always @(posedge PCLK_i) begin
@@ -218,19 +244,13 @@ always @(posedge PCLK_i) begin
     end
 end
 
-// Pipeline stages 1-
+// Pipeline stages 2-
 integer pp_idx;
 always @(posedge PCLK_i) begin
-    for(pp_idx = PP_PL_START+1; pp_idx <= PP_PL_END-1; pp_idx = pp_idx+1) begin
+    for(pp_idx = PP_PL_START+1; pp_idx <= PP_PL_END; pp_idx = pp_idx+1) begin
         R_pp[pp_idx] <= R_pp[pp_idx-1];
         G_pp[pp_idx] <= G_pp[pp_idx-1];
         B_pp[pp_idx] <= B_pp[pp_idx-1];
-    end
-    R_pp[PP_PL_END] <= csc_enable ? R_csc : R_pp[PP_PL_END-1];
-    G_pp[PP_PL_END] <= csc_enable ? G_csc : G_pp[PP_PL_END-1];
-    B_pp[PP_PL_END] <= csc_enable ? B_csc : B_pp[PP_PL_END-1];
-
-    for(pp_idx = PP_PL_START+1; pp_idx <= PP_PL_END; pp_idx = pp_idx+1) begin
         HSYNC_pp[pp_idx] <= HSYNC_pp[pp_idx-1];
         VSYNC_pp[pp_idx] <= VSYNC_pp[pp_idx-1];
         FID_pp[pp_idx] <= FID_pp[pp_idx-1];
@@ -238,6 +258,41 @@ always @(posedge PCLK_i) begin
         datavalid_pp[pp_idx] <= datavalid_pp[pp_idx-1];
         xpos_pp[pp_idx] <= xpos_pp[pp_idx-1];
         ypos_pp[pp_idx] <= ypos_pp[pp_idx-1];
+    end
+
+    /* ---------- CSC (5 cycles) ---------- */
+    R_pp[PP_CSC_END] <= csc_enable ? R_csc : R_pp[PP_CSC_END-1];
+    G_pp[PP_CSC_END] <= csc_enable ? G_csc : G_pp[PP_CSC_END-1];
+    B_pp[PP_CSC_END] <= csc_enable ? B_csc : B_pp[PP_CSC_END-1];
+
+    /* ---------- Reverse LPF (3 cycles) ---------- */
+    // Store a copy of valid sample data
+    if (datavalid_pp[PP_RLPF_START]) begin
+        R_pp_prev_rlpf <= R_pp[PP_RLPF_START];
+        G_pp_prev_rlpf <= G_pp[PP_RLPF_START];
+        B_pp_prev_rlpf <= B_pp[PP_RLPF_START];
+    end
+    // Push previous valid data into pipeline when RLPF enabled
+    if (MISC_REV_LPF_ENABLE) begin
+        R_pp[PP_RLPF_START+1] <= R_pp_prev_rlpf;
+        G_pp[PP_RLPF_START+1] <= G_pp_prev_rlpf;
+        B_pp[PP_RLPF_START+1] <= B_pp_prev_rlpf;
+    end
+    // Calculate diff to previous valid data
+    R_diff_s15_pre <= (R_pp_prev_rlpf - R_pp[PP_RLPF_START]);
+    G_diff_s15_pre <= (G_pp_prev_rlpf - G_pp[PP_RLPF_START]);
+    B_diff_s15_pre <= (B_pp_prev_rlpf - B_pp[PP_RLPF_START]);
+
+    // Cycle 2
+    R_diff_s15 <= (R_diff_s15_pre * MISC_REV_LPF_STR);
+    G_diff_s15 <= (G_diff_s15_pre * MISC_REV_LPF_STR);
+    B_diff_s15 <= (B_diff_s15_pre * MISC_REV_LPF_STR);
+
+    // Cycle 3
+    if (MISC_REV_LPF_ENABLE) begin
+        R_pp[PP_RLPF_END] <= apply_reverse_lpf(R_pp[PP_RLPF_START+2], R_diff_s15);
+        G_pp[PP_RLPF_END] <= apply_reverse_lpf(G_pp[PP_RLPF_START+2], G_diff_s15);
+        B_pp[PP_RLPF_END] <= apply_reverse_lpf(B_pp[PP_RLPF_START+2], B_diff_s15);
     end
 end
 
@@ -297,12 +352,16 @@ always @(posedge CLK_MEAS_i) begin
             meas_hl_det <= 1'b0;
             meas_h_cnt <= 0;
             meas_v_cnt <= meas_v_cnt + 1'b1;
+            if (VSYNC_i_np)
+                meas_vsync_region <= 1'b0;
         end
-    end else if (meas_vblank_region & (meas_h_cnt > pcnt_line)) begin
-        // hsync may be missing during vblank, force line change detect if pcnt_line is exceeded +-1/8 field around vsync edge
+    end else if (meas_vsync_region & (meas_h_cnt >= pcnt_line)) begin
+        // hsync may be missing during vsync, force line change detect if pcnt_line is exceeded
         meas_hl_det <= 1'b0;
         meas_h_cnt <= 0;
         meas_v_cnt <= meas_v_cnt + 1'b1;
+        if (VSYNC_i_np)
+            meas_vsync_region <= 1'b0;
     end else begin
         meas_h_cnt <= meas_h_cnt + 1'b1;
     end
@@ -314,7 +373,7 @@ always @(posedge CLK_MEAS_i) begin
 
             if (vsync_i_type == VSYNC_RAW) begin
                 // vsync leading edge may occur at hsync edge or either side of it
-                if ((HSYNC_i_np_prev & ~HSYNC_i_np) | (meas_h_cnt > pcnt_line)) begin
+                if (HSYNC_i_np_prev & ~HSYNC_i_np) begin
                     meas_v_cnt <= 1;
                     vtotal <= meas_v_cnt;
                 end else if (meas_h_cnt < meas_even_min_thold) begin
@@ -336,6 +395,8 @@ always @(posedge CLK_MEAS_i) begin
                 vtotal <= meas_v_cnt;
             end
         end
+
+        meas_vsync_region <= 1'b1;
     end
 end
 
