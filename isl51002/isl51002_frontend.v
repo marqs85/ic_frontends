312e72c7e1bd6bce5dc01494edd43df5f94d1c91
marqs <mhiienka@niksula.hut.fi>
2023-12-28 23:42:39 +0200

    isl51002: improve sync measurement during vblank

--- isl51002/isl51002_frontend.v
+++ isl51002/isl51002_frontend.v
@@ -128,7 +128,8 @@ wire [11:0] even_max_thold = (vsync_i_type == VSYNC_SEPARATED) ? even_max_thold_
 wire [11:0] meas_even_min_thold = (vsync_i_type == VSYNC_SEPARATED) ? (pcnt_line / 12'd2) : (pcnt_line / 12'd4);
 wire [11:0] meas_even_max_thold = (vsync_i_type == VSYNC_SEPARATED) ? pcnt_line : (pcnt_line / 12'd2) + (pcnt_line / 12'd4);
 
-wire meas_vblank_region = ((pcnt_frame_ctr < (pcnt_frame/16)) | (pcnt_frame_ctr > (pcnt_frame - (pcnt_frame/16))));
+wire meas_vblank_region = (pcnt_frame_ctr < 8*pcnt_line) | (pcnt_frame_ctr > (({1'b0, pcnt_frame}<<interlace_flag) - 4*pcnt_line)) |
+                          (interlace_flag & (pcnt_frame_ctr < (pcnt_frame+8*pcnt_line)) & (pcnt_frame_ctr > (pcnt_frame - 4*pcnt_line)));
 wire [11:0] glitch_filt_thold = meas_vblank_region ? (pcnt_line/4) : (pcnt_line/8);
 
 // TODO: calculate H/V polarity independently
@@ -355,8 +356,8 @@ always @(posedge CLK_MEAS_i) begin
             if (VSYNC_i_np)
                 meas_vsync_region <= 1'b0;
         end
-    end else if (meas_vsync_region & (meas_h_cnt >= pcnt_line)) begin
-        // hsync may be missing during vsync, force line change detect if pcnt_line is exceeded
+    end else if (meas_vblank_region & (meas_h_cnt >= pcnt_line)) begin
+        // hsync may be missing or irregular during vblank, force line change detect if pcnt_line is exceeded
         meas_hl_det <= 1'b0;
         meas_h_cnt <= 0;
         meas_v_cnt <= meas_v_cnt + 1'b1;
